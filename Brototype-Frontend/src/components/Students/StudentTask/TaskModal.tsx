7d12bc297417a13ed9f4d98994ee55bff3fb84b7
officialrashid <muhammedrashi59@gmail.com>
2024-01-26 11:55:49 +0530

    task validation not success

--- Brototype-Frontend/src/components/Students/StudentTask/TaskModal.tsx
+++ Brototype-Frontend/src/components/Students/StudentTask/TaskModal.tsx
@@ -1,10 +1,10 @@
-// TaskModal.js
 import { useFormik } from 'formik';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { updateMiscellaneousWorkout, updatePersonalWorkout, updateTechnicalWorkout } from '../../../utils/methods/post';
 import { toast } from 'react-toastify';
 import { getEditTaskDetails } from '../../../utils/methods/get';
+import * as Yup from 'yup';
 
 interface TaskModalProps {
   isVisible: boolean;
@@ -13,24 +13,29 @@ interface TaskModalProps {
   mainQuestionNumber: number;
   weekName: string;
   taskNumber: number;
-  modalType:string;
-  taskType:string
+  modalType: string;
+  taskType: string;
 }
 
 interface FormValues {
   answers: Record<string, any>;
 }
 
-const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, mainQuestionNumber, weekName, taskNumber,modalType,taskType }) => {
+const ErrorText: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <p className="text-sm font-roboto text-red-700 mt-3 ml-3">
+    {children}
+  </p>
+);
+
+const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, mainQuestionNumber, weekName, taskNumber, modalType, taskType }) => {
   if (!isVisible || !questions || questions.length === 0) return null;
-  const [editData,setEditData] = useState([])
+
+  const [editData, setEditData] = useState([]);
   const studentId: any = useSelector((state: any) => state?.student?.studentData?.studentId);
   const batchId = "657aa5093476c843c28a377d";
+
   useEffect(() => {
     const fetchEditTaskDetails = async () => {
-      console.log(taskType, "task coming useEffect");
-      console.log(modalType, "modal type useEffect");
-  
       try {
         if (modalType === 'Edit') {
           const data = {
@@ -41,22 +46,19 @@ const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, ma
           };
           const response = await getEditTaskDetails(data);
           console.log(response.data[0]);
-          if(response){
-            setEditData(response.data[0])
-          }else{
-            setEditData([])
+          if (response) {
+            setEditData(response.data[0]);
+          } else {
+            setEditData([]);
           }
-         
         }
       } catch (error) {
         console.error("Error fetching edit task details:", error);
       }
     };
-  
-    // Invoke the function here
+
     fetchEditTaskDetails();
-  }, []);  // <-- Make sure to pass an empty dependency array if you want it to run once on mount
-  
+  }, []);
 
   const updateWorkout = async (workoutType: any, workoutData: any) => {
     try {
@@ -74,11 +76,24 @@ const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, ma
     }
   };
 
+  const validationSchema = Yup.object().shape({
+    answers: Yup.object().shape(
+      questions[0]
+        .filter((question: { mainQuestionNumber: number; }) => question.mainQuestionNumber === mainQuestionNumber)
+        .reduce((schema: any, question: { Number: any; }) => {
+          return {
+            ...schema,
+            [question.Number]: Yup.string().trim().required(`Answer for Question ${question.Number} is required`),
+          };
+        }, {})
+    ),
+  });
+
   const formik = useFormik({
     initialValues: {
       answers: {},
     } as FormValues,
-
+    validationSchema: validationSchema,
     onSubmit: async (values) => {
       try {
         console.log('Form values:', values);
@@ -104,12 +119,10 @@ const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, ma
           updateWorkout(updatePersonalWorkout, body);
         } else if (taskNumber === 2) {
           console.log("fbhvbcxhbvhx");
-          
           updateWorkout(updateTechnicalWorkout, body);
         } else if (taskNumber === 3) {
           updateWorkout(updateMiscellaneousWorkout, body);
         }
-
       } catch (err) {
         console.error('Error submitting form:', err);
       }
@@ -117,82 +130,77 @@ const TaskModal: React.FC<TaskModalProps> = ({ isVisible, onclose, questions, ma
   });
 
   const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>, questionNumber: string) => {
+    console.log(e.target.value,"question anser cominggg");
+    
     formik.setFieldValue(`answers.${questionNumber}`, e.target.value);
   };
 
   return (
     <div className="fixed inset-0 bg-opacity-10 bg-black/60 flex justify-center items-center overflow-y-scroll overflow-hidden z-40">
       <div className="border border-gray-200 m-5 rounded-lg shadow-2xl w-3/5 bg-white">
-        <div className="flex justify-between">
-          <div></div>
-          <div className="mr-4 mt-4">
-            <span></span>
-          </div>
-        </div>
-        <div className="text-center">
-          <div>
-            <span className="font-semibold font-roboto text-md mb-2">Update Your Task</span>
-          </div>
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+          {questions[0].map((question: any, index: number) =>
+            question.mainQuestionNumber === mainQuestionNumber && (
+              <div key={question.Number} className="m-5 mt-6">
+                <div className="m-7 border border-black rounded-md shadow-2xl">
+                  <div className="flex justify-between items-center m-4">
+                    <div>
+                      <span className="font-bold font-roboto">
+                        {question.Number}.{question.question}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="m-7 mt-6">
+                  {editData && editData.length > index ? (
+                    <div>
+                      <input
+                        type="text"
+                        id={question.Number}
+                        name={question.Number}
+                        onChange={(e) => handleQuestionChange(e, question?.Number)}
+                        value={editData[index]?.answer}
+                        className="border border-2px rounded-lg text-sm font-roboto pl-3 outline-none shadow-lg w-full py-5"
+                        placeholder={`Enter your answer for Question ${question?.Number}`}
+                      />
 
-
-<form onSubmit={formik.handleSubmit}>
-  {questions[0].map((question: any, index: number) =>
-    question.mainQuestionNumber === mainQuestionNumber && (
-      <div key={question.Number} className="m-5 mt-6">
-        <div className="m-7 border border-black rounded-md shadow-2xl">
-          <div className="flex justify-between items-center m-4">
+                      {formik.touched.answers && formik.errors.answers && formik.errors.answers[question.Number] && (
+                        <ErrorText>{formik?.errors?.answers[question?.Number]}</ErrorText>
+                      )}
+                    </div>
+                  ) : (
+                    <div>
+                      <input
+                        type="text"
+                        id={question.Number}
+                        name={question.Number}
+                        onChange={(e) => handleQuestionChange(e, question.Number)}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.answers[question.Number] || ''}
+                        className="border border-2px rounded-lg text-sm font-roboto pl-3 outline-none shadow-lg w-full py-5"
+                        placeholder={`Enter your answer for Question ${question.Number}`}
+                      />
+                      {formik.touched.answers && formik.errors.answers && formik.errors.answers[question.Number] && (
+                        <ErrorText>{formik.errors.answers[question.Number]}</ErrorText>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </div>
+            )
+          )}
+          <div className="flex justify-between mr-12 m-6 gap">
+            <div></div>
             <div>
-              <span className="font-bold font-roboto">
-                {question.Number}.{question.question}
-              </span>
+              <button className="border px-4 py-1 rounded-md bg-black text-white font-robot" onClick={onclose}>
+                Cancel
+              </button>
+              <button type="submit" className="border px-4 py-1 rounded-md bg-black text-white font-roboto">
+                Submit
+              </button>
             </div>
           </div>
-        </div>
-        <div className="m-7 mt-6">
-          {editData && editData.length > index ? (
-            <input
-              type="text"
-              id={question.Number}
-              name={question.Number}
-              onChange={(e) => handleQuestionChange(e, question.Number)}
-              onBlur={formik.handleBlur}
-              value={editData[index].answer || ''}
-              className="border border-2px rounded-lg text-sm font-roboto pl-3 outline-none shadow-lg w-full py-5"
-              placeholder={`Enter your answer for Question ${question.Number}`}
-            />
-          ) : (
-            <input
-              type="text"
-              id={question.Number}
-              name={question.Number}
-              onChange={(e) => handleQuestionChange(e, question.Number)}
-              onBlur={formik.handleBlur}
-              value={formik.values.answers[question.Number] || ''}
-              className="border border-2px rounded-lg text-sm font-roboto pl-3 outline-none shadow-lg w-full py-5"
-              placeholder={`Enter your answer for Question ${question.Number}`}
-            />
-          )}
-        </div>
-      </div>
-    )
-  )}
-
-  <div className="flex justify-between mr-12 m-6 gap">
-    <div></div>
-    <div>
-      <button className="border px-4 py-1 rounded-md bg-black text-white font-robot" onClick={onclose}>
-        Cancel
-      </button>
-      <button type="submit" className="border px-4 py-1 rounded-md bg-black text-white font-roboto">
-        Submit
-      </button>
-    </div>
-  </div>
-</form>
-
-
-
+        </form>
       </div>
     </div>
   );
